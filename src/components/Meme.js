90f22e591573a02409384ffb05d1c59118f86a8f
Amit Chavda <amitchavda9879@gmail.com>
2022-07-31 22:02:33 +0530

    Genreate Meme done

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,9 +3,27 @@ import MemeAdpter from "../adapters/MemeAdpter"
 import { useState } from "react";
 export default function Meme() {
 
-    const [memeUrl, setMemeUrl] = useState("https://i.imgflip.com/9sw43.jpg");
+    const [memeData, setMemeData] = useState({
+        topText: "",
+        bottomText: "",
+        url: "https://i.imgflip.com/9sw43.jpg"
+    })
+
     function changeMeme() {
-        setMemeUrl(MemeAdpter[Math.floor(Math.random() * 99)].url)
+        setMemeData((prevData) => {
+            return {
+                ...prevData,
+                url: MemeAdpter[Math.floor(Math.random() * 99)].url
+            }
+        })
+    }
+    function handleOnChange(event) {
+        setMemeData((prevData) => {
+            return {
+                ...prevData,
+                [event.target.name]: event.target.value
+            }
+        })
     }
 
     return (
@@ -13,16 +31,28 @@ export default function Meme() {
             <div className="form">
                 <input type="text"
                     className="form-input"
+                    name="topText"
+                    value={memeData.topText}
+                    onChange={handleOnChange}
                     placeholder="Top text" />
+
                 <input type="text"
                     className="form-input"
+                    name="bottomText"
+                    value={memeData.bottomText}
+                    onChange={handleOnChange}
                     placeholder="Bottom text" />
                 <button className="form-button" onClick={changeMeme}>
                     Get a new meme image
                 </button>
             </div>
-            <img className="meme-image"
-                src={memeUrl} />
+
+            <div className="meme">
+                <img className="meme-image"
+                    src={memeData.url} />
+                <h2 className="top-text">{memeData.topText}</h2>
+                <h2 className="bottom-text">{memeData.bottomText}</h2>
+            </div>
         </main>
     )
 }
